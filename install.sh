f293b45bb94fa0618fd5e0d79d3cb8211a1cd994
root <root@localhost.localdomain>
2021-08-25 12:23:54 -0400

    updated install.sh

--- install.sh
+++ install.sh
@@ -3,14 +3,30 @@ id "$username"
 if [ $? -eq 0 ]
 then
 	echo "user exists"
-	exit 1
 else
 	echo "no such user"
 fi
-sudo useradd -s /bin/bash -G sudo -m "$username"
-sudo passwd $username
+sudo useradd -s /bin/bash -m "$username"
+if [ `grep $username /etc/sudoers | wc -l` -eq 0 ]
+then
+	sudo echo "$username ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+	sudo passwd $username
+fi
 echo 'test -d ~/.ssh' | sudo su $username
 if [ $? -eq 1 ]
 then
-	mkdir 
-echo "pip3 install pyttsx3" 
+	echo 'mkdir ~/.ssh' | sudo su $username
+fi
+
+if [ `yum > /tmp/testing; echo $?` -eq 0 ]
+then
+	echo "sudo yum install espeak-ng python36 awscli -y;sudo pip3 install pyttsx3 boto boto3" | sudo su $username
+elif [ `apt-get > /tmp/testing; echo $?` -eq 1 ]
+then
+	echo "sudo apt-get install espeak-ng python3 awscli -y;sudo pip3 install pyttsx3 boto boto3" | sudo su $username
+else
+	echo -e "\nInstall the following softwares manually:\n"
+	echo -e "\t\tespeak-ng, python3, and awscli"
+	echo -e "\nAlso install the following python packages\n"
+	echo -e "\t\tpyttsx3 boto boto3"
+fi
